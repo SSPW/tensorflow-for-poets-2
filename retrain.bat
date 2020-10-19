69c8566ec00b8b22008e01941c1c5bf588185ce4
cravenjw <cravenjw@hotmail.com>
2020-10-19 17:06:43 -0400

    improved scripts

--- retrain.bat
+++ retrain.bat
@@ -1,1 +1,1 @@
-py -3.5 -m scripts.retrain --how_many_training_steps=%1 --learning_rate=%2 --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture=mobilenet_0.50_224 --image_dir=tf_files/flower_photos --summaries_dir=C:\tmp\retrain_logs
+python -m scripts.retrain --how_many_training_steps=%1 --learning_rate=%2 --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture=mobilenet_0.50_224 --image_dir=tf_files/flower_photos --print_misclassified_test_images
